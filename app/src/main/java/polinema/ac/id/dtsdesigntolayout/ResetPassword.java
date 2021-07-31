5ffd2ff3f0ade8d623eec8ed4e00037909772859
od3ng <noprianto@polinema.ac.id>
2021-08-01 06:50:21 +0700

    migreate to androidx

--- app/src/main/java/polinema/ac/id/dtsdesigntolayout/ResetPassword.java
+++ app/src/main/java/polinema/ac/id/dtsdesigntolayout/ResetPassword.java
@@ -1,7 +1,7 @@
 package polinema.ac.id.dtsdesigntolayout;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
--- app/src/main/java/polinema/ac/id/dtsdesigntolayout/SuccessActivity.java
+++ app/src/main/java/polinema/ac/id/dtsdesigntolayout/SuccessActivity.java
@@ -1,6 +1,6 @@
 package polinema.ac.id.dtsdesigntolayout;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SuccessActivity extends AppCompatActivity {
